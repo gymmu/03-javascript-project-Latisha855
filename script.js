17916a83a2c6b285ac19249c5e182e43bc0a37a8
Latisha <latisha.carvalho02@hotmail.com>
2023-10-30 11:40:30 +0100

    Button hinzugefügt

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+
+    // Wenn das aktuelle Zeichen ein 'e' ist, hänge nichts an die Liste an
+if (currentElement=== 'e' ) {
+  result.push ("")
+  // Wenn das aktuelle Zeichen ein 'E' ist, hänge nichts an die Liste an
+} else if (currentElement === 'E') {
+  result.push("")
+}
+else { // Das aktuelle Zeichen ist weder ein 'e' noch ein 'E', hänge also das aktuelle Zeichen
     result.push(currentElement)
+  }
+}
+  return result.join("")
+  
+}
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    // Wandle das aktuelle Element in einen Grossbuchstaben um
+    const capitalizedElement = currentElement.toUpperCase ()
+
+    // Hänge das Grossbuchstaben an das Resultat an...
+    resultat.push (capitalizedElement)
   }
   return result.join("")
 }
