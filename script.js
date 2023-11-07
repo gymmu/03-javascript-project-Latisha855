86f08cf9b5601df5e8010014898fa388e57912fa
Latisha <latisha.carvalho02@hotmail.com>
2023-11-07 09:27:25 +0100

    Aufgabe 3,5,6 teilweise beendet

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ else { // Das aktuelle Zeichen ist weder ein 'e' noch ein 'E', hänge also das a
   
 }
 
+
 export function aufgabe02(args) {
   const input = args
   const result = []
@@ -36,9 +37,11 @@ export function aufgabe02(args) {
   return result.join("")
 }
 
+
 export function aufgabe03(args) {
   const input = args
   const result = [] 
+  let count = 0 
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
@@ -46,11 +49,89 @@ export function aufgabe03(args) {
   // Können Sie überprüfen,ob ein 'e' enthalten ist
 
   if (currentElement=== 'e' ) {
-    result.push ("1")
+   count = count + 1 
+  }
 
-    //
+  //Soll grosses 'E' zählen
+  if (currentElement=== 'E') {
+    count = count + 1 
   }
+
   }
+  return count
+}
+
+
+export function aufgabe04 (args) {
+  const input = args
+  const result = []
   
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  // Können Sie 4 Wörter zählen
+
+
+
+    
+  }
   return result.join("")
 }
+
+export function aufgabe05(args) {
+  const input = args
+    const result = []
+  let capitalLetters = 0
+   
+   
+  for (let i = 0; i < input.length; i++) {
+  const currentElement = input[i]
+   
+  //Erkenne ein Grossbuchstaben
+  if (currentElement=== ".") {}
+  else if (currentElement===" "){}
+   
+  //Erkenne mehrere Grossbuchstaben 
+  else if (currentElement === currentElement.toUpperCase())
+  {capitalLetters++}
+  }
+  // Erkenne den Grossbuchstaben, auch wenn er nicht gross geschrieben ist.
+  if (capitalLetters > 0) {
+    return true
+  } else {
+     return false
+  }
+  // Sollte keine Sonderzeichen enthalten
+  
+   
+  }
+
+  export function aufgabe06 (args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+  
+      // Wandle das aktuelle Element in einen Grossbuchstaben um
+      const capitalizedElement = currentElement.toUpperCase()
+  
+      // Hänge das Grossbuchstaben an das Resultat an...
+      result.push(capitalizedElement)
+      if(currentElement=== )
+    }
+    return result.join("")
+  }
+  
+  
+    
+    return result.join("")
+  }
+
+  export function aufgabe07 (args) {
+    const input = args
+    const result = []
+  
+    
+    return result.join("")
+  }
